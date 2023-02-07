931b28a3a10ae55adda606dc99c36b908806fa8e
Miran Korkmaz <mirankorkmaz@outlook.com>
2023-02-07 20:51:26 +0100

    Add generic sizing

--- main.js
+++ main.js
@@ -22,9 +22,14 @@ const material = new THREE.MeshStandardMaterial({
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
+// Size for screen
+const size = {
+  width: window.innerWidth,
+  height: window.innerHeight
+};
+
 /*
   Add lights to see the globe
-
 */
 const light = new THREE.PointLight(0xffffff, 1, 100);
 light.position.set(0, 10, 10);
@@ -35,13 +40,15 @@ scene.add(light);
   It is basically what we see
   First parameter: FOV (Field of view)
   Second parameter: Aspect-ratio 
+  Camera position is how close the item is to the camera,
+  i.e if it is 30, the globe will be closer to camera
 */
-const camera = new THREE.PerspectiveCamera(45, 800 / 600);
+const camera = new THREE.PerspectiveCamera(45, size.width / size.height);
 camera.position.z = 20;
 scene.add(camera);
 
 // Renderer
 const canvas = document.querySelector('.webgl');
 const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(800, 600);
+renderer.setSize(size.width, size.height);
 renderer.render(scene, camera);
