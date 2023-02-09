752aa7c2db874db5c73443eed13503a7fe8d9090
Miran Korkmaz <mirankorkmaz@outlook.com>
2023-02-09 11:51:31 +0100

    Add timeline

--- main.js
+++ main.js
@@ -81,3 +81,7 @@ const loop = () => {
   window.requestAnimationFrame(loop)
 };
 loop();
+
+// Timeline
+const tl = gsap.timeline({ defaults: { duration: 1} });
+tl.fromTo(mesh.scale, { z:0, x:0, y:0 }, { z:1, x:1, y:1});
